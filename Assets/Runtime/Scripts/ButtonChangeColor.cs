cbede7d2f9b257d954867e36a9ac2e236cc3dac5
Dfam92 <dfam92@hotmail.com>
2022-05-14 13:39:53 -0300

    Adding Images and Scripts
    Adding Images  and Scripts to automatize the color process.

--- Assets/Runtime/Scripts/ButtonChangeColor.cs
+++ Assets/Runtime/Scripts/ButtonChangeColor.cs
@@ -8,31 +8,35 @@ using DG.Tweening;
 
 public class ButtonChangeColor : MonoBehaviour
 {
-    [SerializeField] Button button;
+    //[SerializeField] Button button;
     [SerializeField] Button roseFlowerButton;
     [SerializeField] Image colorPickedByPalete;
+    private Button button;
     // Start is called before the first frame update
     void Start()
     {
-       
-        
+        button = GetComponent<Button>();
+        button.onClick.AddListener(ChangeColor);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        
     }
+    
 
     public void ChangeColor()
     {
-        button.image.DOColor(colorPickedByPalete.color,3);
-        button.image.DOFillAmount(3, 2);
+        this.button.image.DOColor(colorPickedByPalete.color,3);
+        this.button.image.DOFillAmount(3, 2);
+        
     }
 
-    public void ChangeRoseInternalColor()
+   /* public void ChangeRoseInternalColor()
     {
         roseFlowerButton.image.DOColor(colorPickedByPalete.color, 3);
         roseFlowerButton.image.DOFillAmount(3, 2);
-    }
+    }*/
 }
